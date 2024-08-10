0b3ac660e2125bd62ee8abebe6c770884031f70d
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-08-10 14:57:12 +0530

    Update caption.py

--- caption.py
+++ caption.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 API_ID = '22710783'
 API_HASH = '616ea341acfed51f916506c20b8a0a44'
-BOT_TOKEN = '7212369479:AAHD0FfwXkzWcax_hjbMJ1xA4VLaI9mKjtg'
+BOT_TOKEN = '7391520101:AAF0pA1SByHcJV2tDEMSQtA4RMJOkDyOBSA'
 MONGO_URI = "mongodb+srv://test:test@cluster0.q9llhnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['telegram_bot']
