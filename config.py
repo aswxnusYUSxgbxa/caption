4fb48825837099d726c5a6bd917159e2ab8f2800
DrewLikesCode <155294690+aswxnusYUSxgbxa@users.noreply.github.com>
2026-03-12 19:40:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_ID = '22710783'
 API_HASH = '616ea341acfed51f916506c20b8a0a44'
 BOT_TOKEN = '7212369479:AAHD0FfwXkzWcax_hjbMJ1xA4VLaI9mKjtg'
-MONGO_URI = "mongodb+srv://test:test@cluster0.q9llhnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://retryWrites=true&w=majority&appName=Cluster0"
